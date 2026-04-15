ea7c923314b9e6ba9a8c433273fd1783075e0497
IliHanSoLow <ilian-ode@gmx.de>
2026-04-15 22:16:43 +0200

    Added ores, smelting todo

---
+++ silicon_dust/silicon-ore-particle.lua
@@ -0,0 +1,76 @@
+data:extend({
+    {
+        type = "optimized-particle",
+        name = "silicon-ore-particle",
+        flags = { "not-on-map" },
+        life_time = 180,
+        pictures = {
+            {
+                filename = "__factorio_planet_mod__/assets/dummy_silicon_ore.png", -- TODO: need to change these to better file
+                priority = "extra-high",
+                width = 32,
+                height = 32,
+                frame_count = 1,
+                scale = 0.5,
+            },
+            {
+                filename = "__factorio_planet_mod__/assets/dummy_silicon_ore.png", -- TODO: need to change these to better file
+                priority = "extra-high",
+                width = 32,
+                height = 32,
+                frame_count = 1,
+                scale = 0.5,
+            },
+            {
+                filename = "__factorio_planet_mod__/assets/dummy_silicon_ore.png", -- TODO: need to change these to better file
+                priority = "extra-high",
+                width = 32,
+                height = 32,
+                frame_count = 1,
+                scale = 0.5,
+            },
+            {
+                filename = "__factorio_planet_mod__/assets/dummy_silicon_ore.png", -- TODO: need to change these to better file
+                priority = "extra-high",
+                width = 32,
+                height = 32,
+                frame_count = 1,
+                scale = 0.5,
+            },
+        },
+        shadows = {
+            {
+                filename = "__factorio_planet_mod__/assets/dummy_silicon_ore.png", -- TODO: need to change these to better file
+                priority = "extra-high",
+                width = 32,
+                height = 32,
+                frame_count = 1,
+                scale = 0.5,
+            },
+            {
+                filename = "__factorio_planet_mod__/assets/dummy_silicon_ore.png", -- TODO: need to change these to better file
+                priority = "extra-high",
+                width = 32,
+                height = 32,
+                frame_count = 1,
+                scale = 0.5,
+            },
+            {
+                filename = "__factorio_planet_mod__/assets/dummy_silicon_ore.png", -- TODO: need to change these to better file
+                priority = "extra-high",
+                width = 32,
+                height = 32,
+                frame_count = 1,
+                scale = 0.5,
+            },
+            {
+                filename = "__factorio_planet_mod__/assets/dummy_silicon_ore.png", -- TODO: need to change these to better file
+                priority = "extra-high",
+                width = 32,
+                height = 32,
+                frame_count = 1,
+                scale = 0.5,
+            },
+        },
+    },
+})
---
+++ silicon_dust/silicon-ore.lua
@@ -0,0 +1,93 @@
+local resource_autoplace = require("resource-autoplace")
+local util = require("__bztitanium__.data-util")
+local item_sounds = require("__base__.prototypes.item_sounds")
+
+data.raw.planet.mithras.map_gen_settings.autoplace_controls["silicon-ore"] = {}
+data.raw.planet.mithras.map_gen_settings.autoplace_settings.entity.settings["silicon-ore"] = {}
+
+data:extend({
+    {
+        type = "autoplace-control",
+        category = "resource",
+        name = "silicon-ore",
+        richness = true,
+        order = "a-t",
+    },
+    --{
+    --  type = "noise-layer",
+    --  name = "silicon-ore"
+    --},
+    {
+        type = "resource",
+        icon_size = 64,
+        icon_mipmaps = 3,
+        name = "silicon-ore",
+        icon = "__factorio_planet_mod__/assets/silicia-ore-ground.png",
+        flags = { "placeable-neutral" },
+        order = "a-b-a",
+        map_color = { r = 1, g = 1, b = 1 },
+        minable = {
+            hardness = 1,
+            mining_particle = "silicon-ore-particle",
+            mining_time = 2,
+            result = "silicon-ore",
+        },
+        collision_box = { { -0.1, -0.1 }, { 0.1, 0.1 } },
+        selection_box = { { -0.5, -0.5 }, { 0.5, 0.5 } },
+
+        autoplace = resource_autoplace.resource_autoplace_settings({
+            name = "silicon-ore",
+            order = "a-t",
+            base_density = mods["space-age"] and 5 or 3,
+            has_starting_area_placement = false,
+            regular_rq_factor_multiplier = 0.8,
+        }),
+
+        stage_counts = { 15000, 9500, 5500, 2900, 1300, 400, 150, 80 },
+        stages = {
+            sheet = {
+                filename = "__factorio_planet_mod__/assets/silicia-ore-ground.png",
+                priority = "extra-high",
+                size = 128,
+                frame_count = 8,
+                variation_count = 8,
+                scale = 0.5,
+            },
+        },
+    },
+    {
+        type = "item",
+        name = "silicon-ore",
+        icon_size = 64,
+        icon_mipmaps = 3,
+        icon = "__factorio_planet_mod__/assets/silicia-ore-ground.png",
+        inventory_move_sound = item_sounds.resource_inventory_move,
+        pick_sound = item_sounds.resource_inventory_pickup,
+        drop_sound = item_sounds.resource_inventory_move,
+        pictures = {
+            {
+                filename = "__factorio_planet_mod__/assets/silicia-ore-ground.png",
+                size = 64,
+                scale = 0.5,
+            },
+            {
+                filename = "__factorio_planet_mod__/assets/silicia-ore-ground.png",
+                size = 64,
+                scale = 0.5,
+            },
+            {
+                filename = "__factorio_planet_mod__/assets/silicia-ore-ground.png",
+                size = 64,
+                scale = 0.5,
+            },
+            {
+                filename = "__factorio_planet_mod__/assets/silicia-ore-ground.png",
+                size = 64,
+                scale = 0.5,
+            },
+        },
+        subgroup = "raw-resource",
+        order = "t-c-a",
+        stack_size = util.get_stack_size(50),
+    },
+})
