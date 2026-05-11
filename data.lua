b4f17ce9f88c11634136a96b21d6c2739112944f
IliHanSoLow <ilian-ode@gmx.de>
2026-05-11 14:41:04 +0200

    added corse sand to data.lua

--- data.lua
+++ data.lua
@@ -7,6 +7,7 @@ require("silicon_dust.silicon-ore")
 require("silicon_dust.silicon-ore-particle")
 require("silicon_dust.silicon-recipes")
 require("short_inserter.data")
+require("corse_sand.data")
 
 local assemblingMachine4 = table.deepcopy(data.raw["assembling-machine"]["assembling-machine-3"]) -- copy the table that defines the heavy armor item into the fireArmor variable
 
