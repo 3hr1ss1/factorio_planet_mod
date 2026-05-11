93a6c800be8cee4c55c4d7a763203444ae7fde1a
IliHanSoLow <ilian-ode@gmx.de>
2026-05-11 14:30:12 +0200

    added corse-sand as ore and item. no miner yet.

---
+++ corse_sand/corse-sand-ore.lua
@@ -0,0 +1,82 @@
+local resource_autoplace = require("resource-autoplace")
+local item_sounds = require("__base__.prototypes.item_sounds")
+
+-- TODO: Comment this out when the miner is implemented
+data.raw.planet.mithras.map_gen_settings.autoplace_controls["silicon-ore"] =
+    { freqency = 6767, size = 1, richness = 1 }
+data.raw.planet.mithras.map_gen_settings.autoplace_settings.entity.settings["silicon-ore"] =
+    { freqency = 6767, size = 1, richness = 1 }
+
+data:extend({
+    {
+        type = "autoplace-control",
+        category = "resource",
+        name = "corse-sand",
+        richness = true,
+        order = "a-t",
+    },
+    --{
+    --  type = "noise-layer",
+    --  name = "silicon-ore"
+    --},
+    {
+        type = "resource",
+        icon_size = 64,
+        icon_mipmaps = 3,
+        name = "silicon-ore",
+        icon = "__factorio_planet_mod__/assets/silicia_ore.png",
+        flags = { "placeable-neutral" },
+        order = "a-b-a",
+        map_color = { r = 1, g = 1, b = 1 },
+        minable = {
+            hardness = 1,
+            mining_particle = "corse-sand-particle",
+            mining_time = 2,
+            result = "corse-sand",
+        },
+        collision_box = { { -0.1, -0.1 }, { 0.1, 0.1 } },
+        selection_box = { { -0.5, -0.5 }, { 0.5, 0.5 } },
+
+        -- TODO: Comment this out when the miner is implemented
+        autoplace = resource_autoplace.resource_autoplace_settings({
+            name = "corse-sand",
+            order = "a-t",
+            base_density = 8,
+            frequency_multiplier = 2,
+            has_starting_area_placement = false,
+            regular_rq_factor_multiplier = 0.8,
+        }),
+
+        stage_counts = { 15000, 9500, 5500, 2900, 1300, 400, 150, 80 },
+        stages = {
+            sheet = {
+                filename = "__factorio_planet_mod__/assets/silicia-ore-ground.png",
+                priority = "extra-high",
+                size = 128,
+                frame_count = 8,
+                variation_count = 8,
+                scale = 0.45,
+            },
+        },
+    },
+    {
+        type = "item",
+        name = "corse-sand",
+        icon_size = 64,
+        icon_mipmaps = 3,
+        icon = "__factorio_planet_mod__/assets/silicia_ore.png",
+        inventory_move_sound = item_sounds.resource_inventory_move,
+        pick_sound = item_sounds.resource_inventory_pickup,
+        drop_sound = item_sounds.resource_inventory_move,
+        pictures = {
+            {
+                filename = "__factorio_planet_mod__/assets/silicia_ore.png",
+                size = 64,
+                scale = 0.5,
+            },
+        },
+        subgroup = "raw-resource",
+        order = "t-c-a",
+        stack_size = 50,
+    },
+})
